0add92b245d9b93f6a2aa1253c23cd57bb74d532
TanayRayavarapu <60719508+TANAY101405@users.noreply.github.com>
2022-04-05 00:54:47 +0000

    added numberguesser from crossover coding

--- main.py
+++ main.py
@@ -1,15 +1,19 @@
 from week0 import ship, matrix, swap, tree
 from week1 import datalists, fibonacci
-from week2 import factorial, math, palindrome
+from week2 import factorial, math, palindrome, numberguesser
+
+#import functions from each week folder
 
 # ------------------------------------------------
 
 border = "=" * 25
 banner = f"\n{border}\nChoose one of the options: \n{border}"
 
+#define each menu type (weeks 0-2) and the main menu
 def menu():
     title = "Tanay Rayavarapu's Menu" + banner
     buildMenu(title, main_menu)
+   
   
 def numbersmenu():
     title = "Function Submenu" + banner
@@ -25,6 +29,7 @@ def visualmenu():
   
 # ------------------------------------------------
 
+#define submenus with each .py file
 main_menu = [
     ["Math and Numbers", numbersmenu],
     ["Data and Words", datamenu],
@@ -52,30 +57,48 @@ visual_menu = [
 # ------------------------------------------------
 
 def buildMenu(banner, options):
+    # header for menu
     print(banner)
+    # build a dictionary from options
     prompts = {0: ["Exit", None]}
     for op in options:
         index = len(prompts)
         prompts[index] = op
+
+    # print menu or dictionary
     for key, value in prompts.items():
         print(key, '->', value[0])
+
+    # get user choice
     choice = input("Type your choice> ")
+
+    # validate choice and run
+    # execute selection
+    # convert to number
     try:
         choice = int(choice)
         if choice == 0:
+            # stop
             return
         try:
+            # try as function
             action = prompts.get(choice)[1]
             action()
         except TypeError:
-            try:
+            try:  # try as playground style
                 exec(open(action).read())
             except FileNotFoundError:
                 print(f"File not found!: {action}")
+            # end function try
+        # end prompts try
     except ValueError:
+        # not a number error
         print(f"Not a number: {choice}")
     except UnboundLocalError:
+        # traps all other errors
         print(f"Invalid choice: {choice}")
-    buildMenu(banner, options)
+    # end validation try
+
+    buildMenu(banner, options)  # recursion, start menu over again
 if __name__ == "__main__":
     menu()
